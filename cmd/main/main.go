7de283bca8b29bd88b7ad5602eeefd01cd61161e
aljazsevnsek <aljaz.sevnsek@gmail.com>
2025-11-23 21:08:44 +0100

    works

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 
 	r "github.com/sanchae/vpsa-naloga05/redovalnica"
-	"github.com/urfave/cli/v3"
+	cli "github.com/urfave/cli/v3"
 )
 
 func main() {
-
 	r.DodajStudenta("63210001", "Ana", "Novak")
 	r.DodajStudenta("63210002", "Boris", "Kralj")
 	r.DodajStudenta("63210003", "Janez", "Novak")
 
-	app := &cli.App{
+	rootCmd := &cli.Command{
 		Name:  "Redovalnica CLI",
 		Usage: "Upravljanje ocen študentov",
-
 		Flags: []cli.Flag{
 			&cli.IntFlag{Name: "stOcen", Value: 3},
 			&cli.IntFlag{Name: "minOcena", Value: 1},
 			&cli.IntFlag{Name: "maxOcena", Value: 10},
 		},
-
 		Commands: []*cli.Command{
 			{
 				Name:  "dodajOceno",
 				Usage: "dodajOceno <vpisna> <ocena>",
-				Action: func(c *cli.Context) error {
-					vpisna := c.Args().Get(0)
-					oc, _ := strconv.Atoi(c.Args().Get(1))
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					vpisna := cmd.Args().Get(0)
+					oc, _ := strconv.Atoi(cmd.Args().Get(1))
 					r.DodajOceno(vpisna, oc)
 					return nil
 				},
@@ -40,8 +38,8 @@ func main() {
 			{
 				Name:  "izpisiVse",
 				Usage: "izpisiVse <vpisna>",
-				Action: func(c *cli.Context) error {
-					vpisna := c.Args().Get(0)
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					vpisna := cmd.Args().Get(0)
 					fmt.Println(r.IzpisVsehOcen(vpisna))
 					return nil
 				},
@@ -49,11 +47,11 @@ func main() {
 			{
 				Name:  "uspeh",
 				Usage: "Izpiše končni uspeh vseh študentov",
-				Action: func(c *cli.Context) error {
+				Action: func(ctx context.Context, cmd *cli.Command) error {
 					r.IzpisiKoncniUspeh(
-						c.Int("minOcena"),
-						c.Int("maxOcena"),
-						c.Int("stOcen"),
+						cmd.Int("minOcena"),
+						cmd.Int("maxOcena"),
+						cmd.Int("stOcen"),
 					)
 					return nil
 				},
@@ -61,7 +59,7 @@ func main() {
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	if err := rootCmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/sanchae/vpsa-naloga05
 
 go 1.25.3
 
-require github.com/urfave/cli/v3 v3.6.1 // indirect
+require github.com/urfave/cli/v3 v3.6.1
